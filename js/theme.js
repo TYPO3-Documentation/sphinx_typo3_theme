b9b14c58ca406cea28ce84a9ec0c25595b5bfc4e
Martin Bless <martin.bless@mbless.de>
2021-03-10 17:15:56 +0100

    Update theme.js, obey .jshintrc, allow multiple .toc

--- js/theme.js
+++ js/theme.js
@@ -1,44 +1,47 @@
 // T3Docs
 
-// Ensure our own namespace
+// ensure our own namespace
 if (typeof window.T3Docs === 'undefined') {
   window.T3Docs = {};
 }
 
-
-// Inject collapsible menu
-function toggleCurrent(event) {
+// toggle expand-collapse state of menu item
+function toggleCurrent(event) { 'use strict';
   event.preventDefault();
-  var link = event.currentTarget.parentElement;
-  var element = link.parentElement;
-  var siblings = element.parentElement.parentElement.querySelectorAll('li.current');
-  for (var i = 0; i < siblings.length; i++) {
+  const link = event.currentTarget.parentElement;
+  const element = link.parentElement;
+  const siblings = element.parentElement.parentElement.querySelectorAll('li.current');
+  for (let i = 0; i < siblings.length; i++) {
     if (siblings[i] !== element) {
       siblings[i].classList.remove('current');
     }
   }
   element.classList.toggle('current');
 }
-function makeMenuExpandable() {
-  var toc = document.querySelector('.toc');
-  var links = toc.getElementsByTagName('a');
-  for (var i = 0; i < links.length; i++) {
-    if (links[i].nextSibling) {
-      var expand = document.createElement('span');
-      expand.classList.add('toctree-expand');
-      expand.addEventListener('click', toggleCurrent, true);
-      links[i].prepend(expand);
+
+// add toggle icon to a-tags of menu items in .toc navigations
+function makeMenuExpandable() { 'use strict';
+  const tocs = document.getElementsByClassName('toc');
+  for (let i = 0; i < tocs.length; i++) {
+    let links = tocs[i].getElementsByTagName('a');
+    for (let ii = 0; ii < links.length; ii++) {
+      if (links[ii].nextSibling) {
+        let expand = document.createElement('span');
+        expand.classList.add('toctree-expand');
+        expand.addEventListener('click', toggleCurrent, true);
+        links[ii].prepend(expand);
+      }
     }
   }
 }
 makeMenuExpandable();
 
 
-// Wrap tables to make them responsive
-function makeTablesResponsive() {
-  var tables = document.querySelectorAll('.rst-content table.docutils');
-  for (var i = 0; i < tables.length; i++) {
-    wrapper = document.createElement('div');
+// wrap tables to make them responsive
+function makeTablesResponsive() { 'use strict';
+  const tables = document.querySelectorAll('.rst-content table.docutils');
+  for (let i = 0; i < tables.length; i++) {
+    const wrapper = document.createElement('div');
     wrapper.classList.add('table-responsive');
     tables[i].parentNode.insertBefore(wrapper, tables[i]);
     wrapper.appendChild(tables[i]);
@@ -47,72 +50,77 @@ function makeTablesResponsive() {
 makeTablesResponsive();
 
 
-// Search
-document.addEventListener('DOMContentLoaded', function () {
-  var searchform = document.getElementById("search-form");
-  var searchinput = document.getElementById("searchinput");
-  if (searchform && searchinput) {
-    autocomplete({
-      input: searchinput,
-      fetch: function (text, update) {
-        if (typeof window.T3Docs.autocomplete === 'undefined') {
-          window.T3Docs.autocomplete = new Array();
-          Object.keys(Search._index.terms).forEach(function (item, index) {
-            window.T3Docs.autocomplete[index] = { label: item };
+/* global autocomplete:false, Search:false */
+if (!!autocomplete && !!Search) {
+  // add autocompletion to search field
+  document.addEventListener('DOMContentLoaded', function () { 'use strict';
+    const searchform = document.getElementById("search-form");
+    const searchinput = document.getElementById("searchinput");
+    if (searchform && searchinput) {
+      autocomplete({
+        input: searchinput,
+        fetch: function (text, update) {
+          if (typeof window.T3Docs.autocomplete === 'undefined') {
+            window.T3Docs.autocomplete = [];
+            Object.keys(Search._index.terms).forEach(function (item, index) {
+              window.T3Docs.autocomplete[index] = { label: item };
+            });
+          }
+          let suggestions = window.T3Docs.autocomplete.filter(function (entry) {
+            return entry.label.toLowerCase().startsWith(text.toLowerCase());
           });
+          update(suggestions);
+        },
+        minLength: 4,
+        emptyMsg: 'No elements found',
+        render: function (item) {
+          const div = document.createElement("div");
+          div.textContent = item.label;
+          return div;
+        },
+        onSelect: function (item) {
+          searchinput.value = item.label;
+          searchform.submit();
         }
-        var suggestions = window.T3Docs.autocomplete.filter(function (entry) {
-          return entry.label.toLowerCase().startsWith(text.toLowerCase());
-        });
-        update(suggestions);
-      },
-      minLength: 4,
-      emptyMsg: 'No elements found',
-      render: function (item) {
-        var div = document.createElement("div");
-        div.textContent = item.label;
-        return div;
-      },
-      onSelect: function (item) {
-        searchinput.value = item.label;
-        searchform.submit();
-      }
-    });
-  }
-});
+      });
+    }
+  });
+}
 
 
-// Version Selector
-$(document).ready(function () {
+jQuery(document).ready(function () { 'use strict';
+
   function setVersionContent(content) {
-    options = document.createElement('dl');
+    const options = document.createElement('dl');
     options.innerHTML = content;
-    versionOptions = document.getElementById("toc-version-options");
+    const versionOptions = document.getElementById("toc-version-options");
     versionOptions.innerHTML = '';
     versionOptions.appendChild(options);
   }
-  versionNode = document.getElementById("toc-version");
+
+  const versionNode = document.getElementById("toc-version");
   if (versionNode) {
     versionNode.addEventListener('click', function () {
-      versionWrapper = document.getElementById("toc-version-wrapper");
+      const versionWrapper = document.getElementById("toc-version-wrapper");
       versionWrapper.classList.toggle('toc-version-wrapper-active');
-      versionOptions = document.getElementById("toc-version-options");
+      const versionOptions = document.getElementById("toc-version-options");
       if (!versionOptions.dataset.ready) {
-        versionsUri = 'https://docs.typo3.org/services/ajaxversions.php?url=' + encodeURI(document.URL);
-        $.ajax({
+        const versionsUri = 'https://docs.typo3.org/services/ajaxversions.php?url=' + encodeURI(document.URL);
+        jQuery.ajax({
           url: versionsUri,
           success: function (result) {
             setVersionContent(result);
-            versionOptions = document.getElementById("toc-version-options");
+            const versionOptions = document.getElementById("toc-version-options");
             versionOptions.dataset.ready = true;
           },
           error: function () {
             setVersionContent('<p>No data available.</p>');
-            versionOptions = document.getElementById("toc-version-options");
+            const versionOptions = document.getElementById("toc-version-options");
             versionOptions.dataset.ready = true;
           }
         });
       }
     });
   }
+
 });
