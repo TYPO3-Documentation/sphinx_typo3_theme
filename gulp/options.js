8b0cfe11588ffa57ca2e831f838a21f267a7e72a
Martin Bless <martin.bless@mbless.de>
2021-03-11 21:34:12 +0100

    [FEATURE] Start using Gulp

---
+++ gulp/options.js
@@ -0,0 +1,29 @@
+exports.modernizrOptions = {
+  'options': [
+    'domPrefixes',
+    'prefixes',
+    'addTest',
+    'hasEvent',
+    'mq',
+    'prefixedCSSValue',
+    'testAllProps',
+    'testProp',
+    'testStyles',
+    'setClasses'
+  ],
+  'feature-detects': [
+    'custom-elements',
+    'history',
+    'pointerevents',
+    'postmessage',
+    'webgl',
+    'websockets',
+    'css/animations',
+    'css/columns',
+    'css/flexbox',
+    'elem/picture',
+    'img/sizes',
+    'img/srcset',
+    'workers/webworkers'
+  ]
+};
